d829f57346a97fb76f1ed2851a4466135ae8fb14
Gary Pang <gary.pang@gmail.com>
2017-01-25 03:36:01 -0500

    Update app.js
    Use GET method to get data from Random User API. Append users' names and
    images to <ul>.

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
+// createnode and append are helper function
+function createNode(element) {
+	// Create the type of element that's passed in the parameter
+	return document.createElement(element);
+}
+
+function append(parent, el) {
+	// Append second parameter (element) to first parameter
+	return parent.appendChild(el);
+}
+
 var ul = document.getElementById('authors'); // Get list where we'll place authors
 var url = 'https://randomuser.me/api/?results=10';  // Get 10 random users
 
 fetch(url)
+	.then(function(res) {
+		// Transform data into JSON
+		return res.json();
+	})
+
 	.then(function(data) {
-		// Get and modify data
+		// Create and append li's to ul
+		var authors = data.results;
+		return authors.map(function(author) {
+			var li = createNode('li'),
+				img = createNode('img'),
+				span = createNode('span');
+			img.src = author.picture.medium;
+			span.innerHTML = author.name.first + ' ' + author.name.last;
+			append(li, img);
+			append(li, span);
+			append(ul, li);
+		});
 	})
 	.catch(function(error) {
 		// Catch any error here
+		console.log(error);
 	});
